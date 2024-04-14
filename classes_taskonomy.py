ba39fccd40082e99d051e7095f1db554419691c4
Max Bardelang <max.bardelang@gmail.com>
2024-04-14 19:55:30 +0200

    "reorganised classes"

--- classes_taskonomy.py
+++ classes_taskonomy.py
@@ -16,88 +16,69 @@ class ImageDatasetTaskonomy(object):
     yield images in same order as corresponding beauty ratings in file (which are according to the alphanmerical order of image names)
     """
     def __init__(self, img_dir, beauty_ratings_path=None):
-        dir_img_list = list(f for f in os.listdir(img_dir) 
-                            if os.path.isfile(os.path.join(img_dir, f)) and f.endswith('.jpg'))
 
-        self.img_dir  = img_dir
-        self.img_list =  sorted(dir_img_list)
-        self.img_count = len(dir_img_list)
+        dir_img_list    = list(f for f in os.listdir(os.path.join(img_dir, 'full')))
+        self.img_dir    = img_dir
+        self.img_list   = sorted(dir_img_list)
+        self.img_count  = len(dir_img_list)
         if beauty_ratings_path is not None:
             self.beauty_ratings = pd.read_csv(beauty_ratings_path, header=None).mean(axis=1)
 
     def __iter__(self, transform = lambda x: x):
-        """Initialize a iterator for one iteration over the dataset in alpha numerical order of image names"""
+        """Iterator for one iteration over the dataset in alpha numerical order of image names"""
         self.img_pos = 0
-        self.transform = transform #option to apply a transformation to all images, applies only for this iterator
-        # ATTENTION: transform is commented out in iterator
-
         return self
     
     def __next__(self):
         if self.img_pos < self.img_count:
-            img = torchvision.io.read_image(os.path.join(self.img_dir, self.img_list[self.img_pos]))
+            # load arrays transformed in matlab
+            img_full = loadmat(os.path.join(self.img_dir,'full', self.img_list[self.img_pos]))["im"]
+            img_v1 = loadmat(os.path.join(self.img_dir,'version1', self.img_list[self.img_pos]))["imv1"]
+            img_v2 = loadmat(os.path.join(self.img_dir,'version2', self.img_list[self.img_pos]))["imv2"]
+            
+            # convert to input format of Taskonomy models
+            img_full = torch.tensor(img_full).permute([2, 0, 1]).unsqueeze(0) 
+            img_v1 = torch.tensor(img_v1).permute([2, 0, 1]).unsqueeze(0) 
+            img_v2 = torch.tensor(img_v2).permute([2, 0, 1]).unsqueeze(0) 
             self.img_pos += 1
-            #return self.transform(img)
-            return img
-        else:
+            return img_full, img_v1, img_v2
+        else: # prepare for a possible next iteration
             self.img_pos = 0
-            self.transform = lambda x: x
             raise StopIteration
-        
-
-class IntegrationCalculatorTaskonomy(object):
-    def __init__(self, net, evaluation_layers_dict: dict):
-        """
-        Initialise with full PyTorch network model and layer names as in:
-        _, eval_nodes = get_graph_node_names(net)
-        
-        ...and in the same structure as in the return_nodes dict in:
-        https://pytorch.org/vision/stable/feature_extraction.html#api-reference
-        
-        """
-        self.net = create_feature_extractor(net, return_nodes=evaluation_layers_dict)
-        self.evalutation_layers = evaluation_layers_dict.keys()
 
 
-    def integration_coeff(self, img):
-        # TODO import image versions from matlab
-        # TODO export activations to matlab for calculations
-        img, img1, img2 = img.unsqueeze(0), img1.unsqueeze(0), img2.unsqueeze(0) 
+def integration_coeff(net, img_full, img_v1, img_v2):
+    """Recieves readily transformed image versions and calculates integration"""    
+    # activations for full image and image parts
+    act_full, act_v1, act_v2 = net(img_full), net(img_v1), net(img_v2)
 
-        
-        # activations for full image and image parts
-        img_act, img1_act, img2_act = self.net(img), self.net(img1), self.net(img2)
-
-        
+    integration = {}
+    for (layer, act_full_, act_v1_, act_v2_) in zip(act_full.keys(), act_full.values(), act_v1.values(), act_v2.values()):
         # average activation for image parts
-        img12avg_act = { layer:None for layer in img_act.keys()}
-        for layer in img_act.keys():
-            img12avg_act[layer] = torch.stack((img1_act[layer], img2_act[layer]), dim=0).mean(dim=0)
-        
+        act_avg_ = torch.stack((act_v1_, act_v2_), dim=0).mean(dim=0).flatten()
+        act_full_ = act_full_.flatten()
+
         # calculate integration coefficient
-        integration = { layer:None for layer in img_act.keys()}
+        integration[layer] = -pearsonr(act_full_, act_avg_)[0]
 
-        for (layer, act1, act2) in zip(integration.keys(), img_act.values(), img12avg_act.values()):
-
-            integration[layer] = -pearsonr(act1.flatten(), act2.flatten())[0]
-
-        return integration
+    return integration
 
 
-def calculate_dataset_integration(dataset_iterator, integration_calculator: IntegrationCalculatorTaskonomy):
+def calculate_dataset_integration(ImageDataset_iterator, net_tweaked):
     """Calculate integration for whole dataset
         Input:
             dataset_iterator: class ImageDataset iterator that optionally specifies a image transformation
-            integration: Integration calculator object that specifies DNN, layer to use and integration calculation procedure
-        
+            net_tweaked: DNN activations extractor 
         Output:
             DataFrame: images x layers
     """
-    lst = []
-    for img in dataset_iterator:
-        lst.append(integration_calculator.integration_coeff(img))
 
-    return pd.DataFrame(lst, columns=integration_calculator.evalutation_layers)
+    lst = []
+    for img_full, img_v1, img_v2 in ImageDataset_iterator:
+        lst.append(integration_coeff(net_tweaked, img_full, img_v1, img_v2))
+    
+    #                                v this is just for retrieving column names v
+    return pd.DataFrame(lst, columns=list(net_tweaked(torch.zeros(1,3,256, 256)).keys())) 
 
 def correlate_integration_beauty(integration_ratings: pd.DataFrame, beauty_ratings: pd.DataFrame):
     return integration_ratings.aggregate(lambda x: spearmanr(x, beauty_ratings)[0], axis= 0)
--- classes_vgg16.py
+++ classes_vgg16.py
@@ -10,103 +10,74 @@ import torchvision.transforms.functional as TF
 from scipy.io import loadmat
 
 
-
-class IntegrationCalculatorVGG16Matfiles(object):
-    def __init__(self, net, evaluation_layers_dict: dict):
-        """
-        Initialise with full PyTorch network model and layer names as in:
-        _, eval_nodes = get_graph_node_names(net)
-        
-        ...and in the same structure as in the return_nodes dict in:
-        https://pytorch.org/vision/stable/feature_extraction.html#api-reference
-        
-        """
-        self.net = create_feature_extractor(net, return_nodes=evaluation_layers_dict)
-        self.evalutation_layers = evaluation_layers_dict.keys()
-
-
-    def integration_coeff(self, img, img1, img2):
-
-        with torch.no_grad():
-            img_act, img1_act, img2_act = self.net(img), self.net(img1), self.net(img2)
-
-            img12avg_act = { layer:None for layer in img_act.keys()}
-            for layer in img_act.keys():
-                img12avg_act[layer] = torch.stack((img1_act[layer], img2_act[layer]), dim=0).mean(dim=0)
-            
-            integration = { layer:None for layer in img_act.keys()}
-            for (layer, act1, act2) in zip(integration.keys(), img_act.values(), img12avg_act.values()):
-                integration[layer] = -pearsonr(act1.flatten(), act2.flatten())[0]
-
-        return integration
-    
-
-# --- extra class for importing preresized matlab files
-class ImageDatasetMatfiles(object):
+class ImageDataset(object):
     """
-    Iterate for Image dataset, ensures to always iterate in same order to
-    yield images in same order as corresponding beauty ratings in file (which are according to the alphanmerical order of image names)
+    Iterator for Image dataset.
+    Ensures to iaterating images in same order as corresponding beauty ratings in file (alphanumerical order).
     """
     def __init__(self, img_dir, beauty_ratings_path=None):
-        dir_img_list = list(f for f in os.listdir(img_dir) 
-                            if os.path.isfile(os.path.join(img_dir, f)) and f.endswith('.mat'))
 
-        self.img_dir  = img_dir
-        self.img_list =  sorted(dir_img_list)
-        self.img_count = len(dir_img_list)
+        dir_img_list    = list(f for f in os.listdir(os.path.join(img_dir, 'full')))
+        self.img_dir    = img_dir
+        self.img_list   = sorted(dir_img_list)
+        self.img_count  = len(dir_img_list)
         if beauty_ratings_path is not None:
             self.beauty_ratings = pd.read_csv(beauty_ratings_path, header=None).mean(axis=1)
 
     def __iter__(self, transform = lambda x: x):
-        """Initialize a iterator for one iteration over the dataset in alpha numerical order of image names"""
+        """Iterator for one iteration over the dataset in alpha numerical order of image names"""
         self.img_pos = 0
-        self.transform = transform #option to apply a transformation to all images, applies only for this iterator
-        # ATTENTION: transform is commented out in iterator
-
         return self
     
     def __next__(self):
         if self.img_pos < self.img_count:
-            #img = torchvision.io.read_image(os.path.join(self.img_dir, self.img_list[self.img_pos]))
-            img = torch.tensor(loadmat(os.path.join(self.img_dir, self.img_list[self.img_pos]))['im'])
-            img = img.permute((2, 0, 1))
-            img = img.unsqueeze(0)
-            img = img.float()
-            img = img.flip(1)
-
-            img1 = torch.tensor(loadmat(os.path.join('./data/stimuli_places1_resized_v1', self.img_list[self.img_pos]))['imv1'])
-            img1 = img1.permute((2, 0, 1))
-            img1 = img1.unsqueeze(0)
-            img1 = img1.float()
-            img1 = img1.flip(1)
-
-            img2 = torch.tensor(loadmat(os.path.join('./data/stimuli_places1_resized_v2', self.img_list[self.img_pos]))['imv2'])
-            img2 = img2.permute((2, 0, 1))
-            img2 = img2.unsqueeze(0)
-            img2 = img2.float()
-            img2 = img2.flip(1)
-
+            # load arrays transformed in matlab
+            img_full = loadmat(os.path.join(self.img_dir,'full', self.img_list[self.img_pos]))["im"]
+            img_v1 = loadmat(os.path.join(self.img_dir,'version1', self.img_list[self.img_pos]))["imv1"]
+            img_v2 = loadmat(os.path.join(self.img_dir,'version2', self.img_list[self.img_pos]))["imv2"]
+            
+            # convert to input format of Taskonomy models
+            img_full = torch.tensor(img_full).permute([2, 0, 1]).unsqueeze(0).float().flip(1)
+            img_v1 = torch.tensor(img_v1).permute([2, 0, 1]).unsqueeze(0).float().flip(1)
+            img_v2 = torch.tensor(img_v2).permute([2, 0, 1]).unsqueeze(0).float().flip(1)
             self.img_pos += 1
-            #return self.transform(img)
-            return img, img1, img2
-        else:
+            return img_full, img_v1, img_v2
+        else: # prepare for a possible next iteration
             self.img_pos = 0
-            self.transform = lambda x: x
             raise StopIteration
 
+
+def integration_coeff(net, img_full, img_v1, img_v2):
+    """Calculate integration coefficient"""    
+    # activations for full image and image parts
+    with torch.no_grad():
+        act_full, act_v1, act_v2 = net(img_full), net(img_v1), net(img_v2)
+
+    integration = {}
+    for (layer, act_full_, act_v1_, act_v2_) in zip(act_full.keys(), act_full.values(), act_v1.values(), act_v2.values()):
+        # average activation for image parts
+        act_avg_ = torch.stack((act_v1_, act_v2_), dim=0).mean(dim=0).flatten()
+        act_full_ = act_full_.flatten()
+
+        # calculate integration coefficient
+        integration[layer] = -pearsonr(act_full_, act_avg_)[0]
+
+    return integration
+
+
+def calculate_dataset_integration(ImageDataset_iterator, net_tweaked):
+    """Calculate integration for whole dataset"""
+    lst = []
+    for img_full, img_v1, img_v2 in ImageDataset_iterator:
+        lst.append(integration_coeff(net_tweaked, img_full, img_v1, img_v2))
     
+    column_names = list(net_tweaked(torch.zeros(1,3,224,224).float().flip(1)).keys())
+    return pd.DataFrame(lst, columns=column_names) 
 
-    def calculate_dataset_integration_Matfiles(dataset_iterator, integration_calculator: IntegrationCalculator):
-        """Calculate integration for whole dataset
-            Input:
-                dataset_iterator: class ImageDataset iterator that optionally specifies a image transformation
-                integration: Integration calculator object that specifies DNN, layer to use and integration calculation procedure
-            
-            Output:
-                DataFrame: images x layers
-        """
-        lst = []
-        for img, img1, img2 in dataset_iterator:
-            lst.append(integration_calculator.integration_coeff(img, img1, img2))
 
-        return pd.DataFrame(lst, columns=integration_calculator.evalutation_layers)
+def correlate_integration_beauty(integration_ratings: pd.DataFrame, beauty_ratings: pd.DataFrame):
+    return integration_ratings.aggregate(lambda x: spearmanr(x, beauty_ratings)[0], axis= 0)
+
+
+
+
