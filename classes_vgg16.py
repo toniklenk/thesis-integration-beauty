f6fc51ed520d7dfb37b81ff3905e59d2015413c4
Max Bardelang <max.bardelang@gmail.com>
2024-04-15 06:22:00 +0200

    "added matrics and configurations"

--- classes_vgg16.py
+++ classes_vgg16.py
@@ -47,7 +47,8 @@ class ImageDataset(object):
             raise StopIteration
 
 
-def integration_coeff(net, img_full, img_v1, img_v2):
+# --- Correlation (-Integration)
+def correlation_coeff(net, img_full, img_v1, img_v2):
     """Calculate integration coefficient"""    
     # activations for full image and image parts
     with torch.no_grad():
@@ -59,25 +60,73 @@ def integration_coeff(net, img_full, img_v1, img_v2):
         act_avg_ = torch.stack((act_v1_, act_v2_), dim=0).mean(dim=0).flatten()
         act_full_ = act_full_.flatten()
 
-        # calculate integration coefficient
-        integration[layer] = -pearsonr(act_full_, act_avg_)[0]
+        integration[layer] = pearsonr(act_full_, act_avg_)[0]
 
     return integration
 
 
-def calculate_dataset_integration(ImageDataset_iterator, net_tweaked):
+def calculate_dataset_correlation(ImageDataset_iterator, net_tweaked):
     """Calculate integration for whole dataset"""
     lst = []
     for img_full, img_v1, img_v2 in ImageDataset_iterator:
-        lst.append(integration_coeff(net_tweaked, img_full, img_v1, img_v2))
+        lst.append(correlation_coeff(net_tweaked, img_full, img_v1, img_v2))
     
     column_names = list(net_tweaked(torch.zeros(1,3,224,224).float().flip(1)).keys())
     return pd.DataFrame(lst, columns=column_names) 
 
 
-def correlate_integration_beauty(integration_ratings: pd.DataFrame, beauty_ratings: pd.DataFrame):
-    return integration_ratings.aggregate(lambda x: spearmanr(x, beauty_ratings)[0], axis= 0)
+def correlate_integration_beauty(correlation_ratings: pd.DataFrame, beauty_ratings: pd.DataFrame):
+    return correlation_ratings.aggregate(lambda x: spearmanr(-x, beauty_ratings)[0], axis= 0)
 
 
+# --- Self similarity
+def self_similarity(net, img_v1, img_v2):
+    """Calculate image self-similarity"""    
+    # activations for image parts
+    with torch.no_grad():
+        act_v1, act_v2 = net(img_v1), net(img_v2)
+
+    selfsimilarity = {}
+    for (layer, act_v1_, act_v2_) in zip(act_v1.keys(), act_v1.values(), act_v2.values()):
+        act_v1_ = act_v1_.flatten()
+        act_v2_ = act_v2_.flatten()
+
+        selfsimilarity[layer] = pearsonr(act_v1_, act_v2_)[0]
+
+    return selfsimilarity
+
+
+def calculate_dataset_self_similarity(ImageDataset_iterator, net_tweaked):
+    """Calculate self-similarity for whole dataset"""
+    lst = []
+    for _, img_v1, img_v2 in ImageDataset_iterator:
+        lst.append(self_similarity(net_tweaked, img_v1, img_v2))
+    
+    column_names = list(net_tweaked(torch.zeros(1,3,224,224).float().flip(1)).keys())
+    return pd.DataFrame(lst, columns=column_names) 
+
+
+# --- L2 norm
+def l2_norm(net, img_full):
+    """Calculate imgage L2-norm"""    
+    # activation for full image
+    with torch.no_grad():
+        act_full = net(img_full)
+
+    l2norm = {}
+    for (layer, act_full_) in zip(act_full.keys(), act_full.values()):
+        l2norm[layer] = act_full_.norm(p=2).item()
+
+    return l2norm
+
+
+def calculate_dataset_l2norm(ImageDataset_iterator, net_tweaked):
+    """Calculate L2-norm for whole dataset"""
+    lst = []
+    for img_full, _, _ in ImageDataset_iterator:
+        lst.append(l2_norm(net_tweaked, img_full))
+    
+    column_names = list(net_tweaked(torch.zeros(1,3,224,224).float().flip(1)).keys())
+    return pd.DataFrame(lst, columns=column_names)
 
 
